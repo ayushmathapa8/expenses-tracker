f9a83eefc2d61e36142ac3830fd84c89aa0269d2
Ayushma Thapa <ayushmathapa@Ayushmas-MacBook-Pro.local>
2022-08-03 14:12:19 +1000

    name changed

---
+++ client-/src/pages/userState/userAction.js
@@ -0,0 +1,21 @@
+import { loginUser } from "../../helpers/axiosHelper";
+import { toast } from "react-toastify";
+import { setUser } from "./userSlice";
+
+export const loginAction = (obj) => async (dispatch) => {
+  // first call axios and get data from the server
+
+  const { status, message, user } = await loginUser(obj);
+  toast[status](message);
+
+  if (status === "success") {
+    window.sessionStorage.setItem("user", JSON.stringify(user));
+    // dispatch the incoming data to the slice
+    dispatch(setUser(user));
+  }
+};
+
+export const userLogoutAction = () => (dispatch) => {
+  dispatch(setUser({}));
+  window.sessionStorage.removeItem("user");
+};
